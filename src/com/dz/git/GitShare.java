197535203a566974f512422d90ca234d0f5025cf
daiyonghui <daiyonghui@xplusz.com>
2018-12-19 18:09:22 +0800

    another dev commit on akali branch

--- src/com/dz/git/GitShare.java
+++ src/com/dz/git/GitShare.java
@@ -2,7 +2,9 @@ package com.dz.git;
 
 public class GitShare {
 	public static void main(String[] args) {
+		String password = "12345";
 		System.out.println("Hello,GitHub");
 		System.out.println("ok");
 	}
 }
+ß
