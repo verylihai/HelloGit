a047c90ec3b899387e0152e9459ed00c8195ac42
daiyonghui <daiyonghui@xplusz.com>
2018-12-20 16:24:14 +0800

    commit by dai at 16:20 pm

--- src/com/dz/git/GitShare.java
+++ src/com/dz/git/GitShare.java
@@ -2,8 +2,10 @@ package com.dz.git;
 
 public class GitShare {
 	public static void main(String[] args) {
-		String username = "tom";
+		String username = "jack";
+		String password = "12345";
 		System.out.println("Hello,GitHub");
 		System.out.println("ok");
 	}
 }
+ß
